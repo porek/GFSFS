05ff12f4fededf0647dd338479e32fdf8a569434
Ben Rosin <brosin.delivers@gmail.com>
2019-04-29 13:45:18 -0400

    Initial Commit

---
+++ MsgTestTests/MsgTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  MsgTestTests.swift
+//  MsgTestTests
+//
+//  Created by Ben Rosin on 4/29/19.
+//  Copyright © 2019 Ben Rosin. All rights reserved.
+//
+
+import XCTest
+@testable import MsgTest
+
+class MsgTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
